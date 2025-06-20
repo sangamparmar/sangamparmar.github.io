e9b59ca54cf36903ddae04410b19211eb1ed1335
sangamparmar <sangamparmar94@gmail.com>
2025-06-20 10:02:58 +0530

    Add new files and update portfolio content

---
+++ file1.js
@@ -0,0 +1,20 @@
+let studentname = "john"
+let age = 21
+let promoted = true
+let score
+let city = null
+// console.log(typeof studentname)
+// console.log(typeof city)
+
+let strage = String(age)
+console.log(typeof strage)
+
+let rating  = "43"
+console.log(typeof rating)
+
+let numrating = Number(rating)
+console.log(typeof numrating)
+
+let flag = "true"
+let boolflag = Boolean(flag)
+console.log(typeof boolflag)
---
+++ file2.js
@@ -0,0 +1,27 @@
+// let a = 30
+// let b = 10
+// console.log(a+b)
+
+// let a = "10"
+// let b = 10
+// console.log(a*b)
+
+// let a = 2
+// let b = 2
+// console.log(a==b)
+
+// let a = "2"
+// let b = 2
+// console.log(a===b)
+
+//issue in this learn diff bte a!=b, a!==b
+// let a = 10
+// let b = 2
+// console.log(a!==b)
+
+let a = 10
+let b = 2
+// let c = 4
+console.log(!a>b)
+// console.log(a>b || b>c)
+// console.log(a>b && b>c)
